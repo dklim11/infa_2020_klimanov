8cde7ea50c7b5c12b2bf6cf6e38494b135a129a4
Klimanov Daniil <klimanov.dd@phytech.edu>
2021-10-05 18:36:53 +0400

    Program is working correctly

--- Lab21/Task9.py
+++ Lab21/Task9.py
@@ -1,9 +1,10 @@
 import turtle
-
 turtle.shape('turtle')
-l = 40
 
-def func(n):
+l = 40
+n = 3
+
+def func(n, l):
 	i = 0
 	turtle.left(180 - 90*(n - 2)/n)
 	while i < n:
@@ -12,13 +13,10 @@ def func(n):
 		i += 1
 	turtle.right(90*(n - 2)/n + 180 - 180*(n - 2)/n)
 	turtle.penup()
-	turtle.forward(l * 0.5)
+	turtle.forward(l * 0.5 - (n - 3) * 5)
 	turtle.pendown()
-	
-n = 3
 
 while n < 10:
-	func(n)
+	func(n, l)
 	l += 20
-	n += 1
-	
+	n += 1 
